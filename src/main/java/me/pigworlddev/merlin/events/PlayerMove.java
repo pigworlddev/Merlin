f2614639be8d98b29981d00b3068093f0de7e93e
ThePigStupo <olannbrink7@gmail.com>
2020-12-19 16:10:00 +0100

    Added a permission that restricts movement.
    Signed-off-by: ThePigStupo <olannbrink7@gmail.com>

---
+++ src/main/java/me/pigworlddev/merlin/events/PlayerMove.java
@@ -0,0 +1,17 @@
+package me.pigworlddev.merlin.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PlayerMove implements Listener {
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasPermission("merlin.jailed")) {
+            event.setCancelled(true);
+        }
+    }
+}
